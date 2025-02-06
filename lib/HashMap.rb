70931e85d8079d654892e1f3d7eab5262c61e957
khabigcorp <99237998+khabigcorp@users.noreply.github.com>
2025-02-06 15:51:14 +0800

    Wrote constructor and hash method for LinkedList

--- lib/HashMap.rb
+++ lib/HashMap.rb
@@ -1,2 +1,18 @@
 require './lib/LinkedList.rb'
 
+class HashMap
+  def intialize(load, capacity)
+    @load = load
+    @capacity = capacity
+  end
+
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+       
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+       
+    hash_code
+  end
+  
+end
